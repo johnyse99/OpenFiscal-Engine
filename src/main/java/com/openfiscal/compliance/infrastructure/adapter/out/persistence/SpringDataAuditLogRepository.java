dafb8e785af77cf8a90a53960ad2d705404b72e2
johnyse99@gmail.com <regiomaster@protonmail.com>
2026-09-22 21:58:10 -0700

    feat(compliance): add JPA entity and Spring Data repository for Immutable Audit Log

---
+++ src/main/java/com/openfiscal/compliance/infrastructure/adapter/out/persistence/SpringDataAuditLogRepository.java
@@ -0,0 +1,30 @@
+/*
+ * OpenFiscal-Engine
+ *
+ * Author: Juan S.
+ * Copyright (C) 2026 Juan S.
+ *
+ * This file is part of OpenFiscal-Engine.
+ *
+ * IFMP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * IFMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenFiscal-Engine. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.openfiscal.compliance.infrastructure.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+/**
+ * Spring Data JPA repository for persisting immutable fraud alerts.
+ */
+public interface SpringDataAuditLogRepository extends JpaRepository<FraudAlertJpaEntity, UUID> {
+}
