7f2c47c0393a640aa9d432dcf77180c5bbc0247e
johnyse99@gmail.com <regiomaster@protonmail.com>
2026-09-23 21:05:15 -0700

    test(core): add spring-security-test and mock user to fix unauthorized integration tests

--- src/test/java/com/openfiscal/core/infrastructure/adapter/in/web/InvoiceControllerIntegrationTest.java
+++ src/test/java/com/openfiscal/core/infrastructure/adapter/in/web/InvoiceControllerIntegrationTest.java
@@ -29,6 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -41,6 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser // Inyecta un usuario autenticado simulado para eludir el 401 en las pruebas
 class InvoiceControllerIntegrationTest {
 
     @Autowired
